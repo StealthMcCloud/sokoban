e3b0d5f79d67379a948472a5e6f5809d6c5b2b8b
Clinton Johnson <dragoon1246224@gmail.com>
2018-08-30 16:15:39 -0400

    Boxes move, stop at walls, and stop at other boxes

--- script.js
+++ script.js
@@ -73,37 +73,73 @@ document.addEventListener('keydown', (event) => {
     const rowPosition = currentPosition.dataset.rowIndex
     const cellPosition = currentPosition.dataset.cellIndex
     switch (event.key) {
-        // case 'ArrowUp':
-        //     let nextPositionUp = Number(rowPosition) - 1;
-        //     let twoPositionUp = Number(rowPosition) - 2;
-        //     let nextMoveUp = document.querySelector("[data-row-index = '" + nextPositionUp + "'][data-cell-index = '" + cellPosition + "']");
-        //     let boxMoveUp = document.querySelector("[data-row-index = '" + nextPositionUp + "'][data-cell-index = '" + twoPositionUp + "']");
-        //     const moveUp = nextMoveUp.dataset.cellType
-        //     if (moveUp === "blankSpace" || moveUp === "floor" || moveUp === "winLocations" || moveUp === "box" || moveUp === "start" || moveUp === "boxOnWinLocation") {
-        //         nextMoveUp.appendChild(rarity);
-        //         currentPosition = nextMoveUp;
-        //         rarity.style.transform = "rotate(-90deg)" + "scaleX(-1)"
-        //     }
-        //     break;
-        // case 'ArrowDown':
-        //     let nextPositionDown = Number(rowPosition) + 1;
-        //     let twoPositionDown = Number(rowPosition) + 2;
-        //     let nextMoveDown = document.querySelector("[data-row-index = '" + nextPositionDown + "'][data-cell-index = '" + cellPosition + "']");
-        //     let boxMoveDown = document.querySelector("[data-row-index = '" + nextPositionDown + "'][data-cell-index = '" + twoPositionDown + "']");
-        //     const moveDown = nextMoveDown.dataset.cellType
-        //     if (moveDown === "blankSpace" || moveDown === "floor" || moveDown === "winLocations" || moveDown === "box" || moveDown === "start" || moveDown === "boxOnWinLocation") {
-        //         nextMoveDown.appendChild(rarity);
-        //         currentPosition = nextMoveDown;
-        //         rarity.style.transform = "rotate(90deg)" + "scaleX(-1)"
-        //     }
-        //     if (moveDown === "box") {
-        //         nextMoveDown.classList.replace("box", "blankSpace")
-        //         nextMoveDown.setAttribute("data-cell-type", "blankSpace")
-        //         nextMoveDown.appendChild(rarity)
-        //         rarity.style.transform = "scaleX(-1)"
-        //         boxMoveDown.setAttribute("data-cell-type", "box")
-        //         boxMoveDown.classList.replace("blankSpace", "box")  || nextMoveDown.classList.replace("winLocations", "boxOnWinLocation")}
-        //     break;
+        case 'ArrowUp':
+            let nextPositionUp = Number(rowPosition) - 1;
+            let twoPositionUp = Number(rowPosition) - 2;
+            let nextMoveUp = document.querySelector("[data-row-index = '" + nextPositionUp + "'][data-cell-index = '" + cellPosition + "']");
+            let boxMoveUp = document.querySelector("[data-row-index = '" + twoPositionUp + "'][data-cell-index = '" + cellPosition + "']");
+            const moveUp = nextMoveUp.dataset.cellType
+        const boxUp = boxMoveUp.dataset.cellType
+            if (moveUp === "blankSpace" || moveUp === "floor" || moveUp === "winLocations" || moveUp === "start") {
+                nextMoveUp.appendChild(rarity);
+                currentPosition = nextMoveUp;
+                rarity.style.transform = "rotate(-90deg)" + "scaleX(-1)"
+            }
+        
+        else if (moveUp === "box" && boxUp === "borderWall" || moveUp === "box" && boxUp === "box" ) {
+        console.log("stop");
+        }
+        
+        else if (moveUp === "box") {
+        nextMoveUp.classList.replace("box", "blankSpace");
+        nextMoveUp.setAttribute("data-cell-type", "blankSpace");
+        nextMoveUp.appendChild(rarity);
+        currentPosition = nextMoveUp;
+        rarity.style.transform = "rotate(-90deg)" + "scaleX(-1)";
+        boxMoveUp.setAttribute("data-cell-type", "box");
+        boxMoveUp.classList.replace("blankSpace", "box");
+        } 
+        
+            break;
+
+        case 'ArrowDown':
+            let nextPositionDown = Number(rowPosition) + 1;
+            let twoPositionDown = Number(rowPosition) + 2;
+            let nextMoveDown = document.querySelector("[data-row-index = '" + nextPositionDown + "'][data-cell-index = '" + cellPosition + "']");
+            let boxMoveDown = document.querySelector("[data-row-index = '" + twoPositionDown + "'][data-cell-index = '" + cellPosition + "']");
+            const moveDown = nextMoveDown.dataset.cellType
+            const boxDown = boxMoveDown.dataset.cellType
+            if (moveDown === "blankSpace" || moveDown === "floor" || moveDown === "winLocations" || moveDown === "start") {
+                nextMoveDown.appendChild(rarity);
+                currentPosition = nextMoveDown;
+                rarity.style.transform = "rotate(90deg)" + "scaleX(-1)"
+            }
+
+            else if (moveDown === "box" && boxDown === "borderWall" || moveDown === "box" && boxDown === "box") {
+                console.log("stop")
+            }
+
+            else if (boxDown === "winLocations") {
+                nextMoveDown.classList.replace("box", "blankSpace");
+                nextMoveDown.setAttribute("data-cell-type", "blankSpace");
+                nextMoveDown.appendChild(rarity);
+                currentPosition = nextMoveDown;
+                rarity.style.transform = "rotate(0deg)";
+                boxMoveDown.setAttribute("data-cell-type", "box");
+                boxMoveDown.classList.replace("winLocations", "box")
+            }
+
+            else if (moveDown === "box") {
+                nextMoveDown.classList.replace("box", "blankSpace");
+                nextMoveDown.setAttribute("data-cell-type", "blankSpace");
+                nextMoveDown.appendChild(rarity);
+                currentPosition = nextMoveDown;
+                rarity.style.transform = "rotate(90deg)" + "scaleX(-1)";
+                boxMoveDown.setAttribute("data-cell-type", "box");
+                boxMoveDown.classList.replace("blankSpace", "box");
+            }
+            break;
+
         case 'ArrowLeft':
             let nextPositionLeft = Number(cellPosition) - 1;
             let twoPositionLeft = Number(cellPosition) - 2;
@@ -117,13 +153,34 @@ document.addEventListener('keydown', (event) => {
                 rarity.style.transform = "rotate(0deg)"
             }
 
-            else if (moveLeft === "box" && boxleft === "borderWall") {
+            else if (moveLeft === "box" && boxLeft === "borderWall" || moveLeft === "box" && boxLeft === "box") {
                 console.log("Stop")
             }
 
+            else if (boxLeft === "winLocations") {
+                nextMoveLeft.classList.replace("box", "blankSpace");
+                nextMoveLeft.setAttribute("data-cell-type", "blankSpace");
+                nextMoveLeft.appendChild(rarity);
+                currentPosition = nextMoveLeft;
+                rarity.style.transform = "rotate(0deg)";
+                boxMoveLeft.setAttribute("data-cell-type", "box");
+                boxMoveLeft.classList.replace("winLocations", "box")
+            }
+            
+            else if (boxLeft === "start") {
+                nextMoveLeft.classList.replace("box", "blankSpace");
+                nextMoveLeft.setAttribute("data-cell-type", "blankSpace");
+                nextMoveLeft.appendChild(rarity);
+                currentPosition = nextMoveLeft;
+                rarity.style.transform = "rotate(0deg)";
+                document.getElementById("start").removeAttribute("id"); 
+                boxMoveLeft.classList.add("box");
+                boxMoveLeft.setAttribute("data-cell-type", "box");
+            }
+            
             else if (moveLeft === "box") {
-                nextMoveleft.classList.replace("box", "blankSpace");
-                nextMoveLeft.setAttribute("data-cell-type", "box");
+                nextMoveLeft.classList.replace("box", "blankSpace");
+                nextMoveLeft.setAttribute("data-cell-type", "blankSpace");
                 nextMoveLeft.appendChild(rarity);
                 currentPosition = nextMoveLeft;
                 rarity.style.transform = "rotate(0deg)";
@@ -131,8 +188,8 @@ document.addEventListener('keydown', (event) => {
                 boxMoveLeft.classList.replace("blankSpace", "box");
             }
             break;
-
-        case 'ArrowRight':
+            
+            case 'ArrowRight':
             let nextPositionRight = Number(cellPosition) + 1;
             let twoPositionRight = Number(cellPosition) + 2;
             let nextMoveRight = document.querySelector("[data-row-index = '" + rowPosition + "'][data-cell-index = '" + nextPositionRight + "']");
@@ -147,17 +204,27 @@ document.addEventListener('keydown', (event) => {
             else if (moveRight === "box" && boxRight === "borderWall") {
                 console.log("Stop");
             }
+            
+            else if (boxRight === "winLocations") {
+                nextMoveRight.classList.replace("box", "blankSpace");
+                nextMoveRight.setAttribute("data-cell-type", "blankSpace");
+                nextMoveRight.appendChild(rarity);
+                currentPosition = nextMoveRight;
+                rarity.style.transform = "scaleX(-1)";
+                boxMoveRight.setAttribute("data-cell-type", "box");
+                boxMoveRight.classList.replace("winLocations", "box")
+            }
 
             else if (moveRight === "box") {
-                nextMoveRight.classList.replace("box", "blankSpace")
-                nextMoveRight.setAttribute("data-cell-type", "blankSpace")
-                nextMoveRight.appendChild(rarity)
+                nextMoveRight.classList.replace("box", "blankSpace");
+                nextMoveRight.setAttribute("data-cell-type", "blankSpace");
+                nextMoveRight.appendChild(rarity);
                 currentPosition = nextMoveRight;
-                rarity.style.transform = "scaleX(-1)"
-                boxMoveRight.setAttribute("data-cell-type", "box")
-                boxMoveRight.classList.replace("blankSpace", "box")
+                rarity.style.transform = "scaleX(-1)";
+                boxMoveRight.setAttribute("data-cell-type", "box");
+                boxMoveRight.classList.replace("blankSpace", "box");
             }
             break;
+        }
     }
-}
 );
