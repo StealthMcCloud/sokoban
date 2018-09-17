f35b108721cfe06c63446f5b8817dc8e1fcface2
Clinton Johnson <dragoon1246224@gmail.com>
2018-09-17 15:18:56 -0400

    Win Condition is working

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+function rightMove(nextMoveRight, boxMoveRight) {
+    nextMoveRight.classList.replace("box", "blankSpace");
+    nextMoveRight.setAttribute("data-cell-type", "blankSpace");
+    nextMoveRight.appendChild(rarity);
+    currentPosition = nextMoveRight;
+    rarity.style.transform = "scaleX(-1)";
+    boxMoveRight.setAttribute("data-cell-type", "box");
+}
+
+function leftMove(nextMoveLeft, boxMoveLeft) {
+    nextMoveLeft.classList.replace("box", "blankSpace");
+    nextMoveLeft.setAttribute("data-cell-type", "blankSpace");
+    nextMoveLeft.appendChild(rarity);
+    currentPosition = nextMoveLeft;
+    rarity.style.transform = "rotate(0deg)";
+}
+
+function upMove(nextMoveUp, boxMoveUp) {
+    nextMoveUp.classList.replace("box", "blankSpace");
+    nextMoveUp.setAttribute("data-cell-type", "blankSpace");
+    nextMoveUp.appendChild(rarity);
+    currentPosition = nextMoveUp;
+    rarity.style.transform = "rotate(-90deg)" + "scaleX(-1)";
+}
+
+function downMove(nextMoveDown, boxMoveDown) {
+    nextMoveDown.classList.replace("box", "blankSpace");
+    nextMoveDown.setAttribute("data-cell-type", "blankSpace");
+    nextMoveDown.appendChild(rarity);
+    currentPosition = nextMoveDown;
+    rarity.style.transform = "rotate(90deg)" + "scaleX(-1)";
+}
+
+let winCounter = 1;
+
 //map layout
 const map = [
     "  WWWWW ",
@@ -10,7 +45,7 @@ const map = [
     "W   O  W",
     "WWWWWWWW"
 ];
-
+//Creates the game map
 const main = document.getElementById("maze")
 
 for (let y = 0; y < map.length; y++) {
@@ -51,7 +86,7 @@ for (let y = 0; y < map.length; y++) {
                 break;
 
             case "X":
-                wall.classList.add("box" || "winLocations");
+                wall.classList.add("winLocations", "box");
                 wall.dataset.cellType = "boxOnWinLocation";
                 break;
         }
@@ -59,49 +94,56 @@ for (let y = 0; y < map.length; y++) {
     main.appendChild(mazeBoard);
 }
 
+
+//Brings player piece rarity over from css
 const rarity = document.getElementById("rarity")
 const box = document.getElementsByClassName("box")
 const border = document.getElementsByClassName("border")
 
+//Places player piece rarity onto starting space
 let start = document.getElementById("start")
 let currentPosition = start;
 currentPosition.appendChild(rarity);
-
-
 
 document.addEventListener('keydown', (event) => {
     const rowPosition = currentPosition.dataset.rowIndex
     const cellPosition = currentPosition.dataset.cellIndex
     switch (event.key) {
+        //moves player up, and pushes boxes
         case 'ArrowUp':
             let nextPositionUp = Number(rowPosition) - 1;
             let twoPositionUp = Number(rowPosition) - 2;
             let nextMoveUp = document.querySelector("[data-row-index = '" + nextPositionUp + "'][data-cell-index = '" + cellPosition + "']");
             let boxMoveUp = document.querySelector("[data-row-index = '" + twoPositionUp + "'][data-cell-index = '" + cellPosition + "']");
             const moveUp = nextMoveUp.dataset.cellType
-        const boxUp = boxMoveUp.dataset.cellType
+            const boxUp = boxMoveUp.dataset.cellType
             if (moveUp === "blankSpace" || moveUp === "floor" || moveUp === "winLocations" || moveUp === "start") {
                 nextMoveUp.appendChild(rarity);
                 currentPosition = nextMoveUp;
                 rarity.style.transform = "rotate(-90deg)" + "scaleX(-1)"
             }
-        
-        else if (moveUp === "box" && boxUp === "borderWall" || moveUp === "box" && boxUp === "box" ) {
-        console.log("stop");
-        }
-        
-        else if (moveUp === "box") {
-        nextMoveUp.classList.replace("box", "blankSpace");
-        nextMoveUp.setAttribute("data-cell-type", "blankSpace");
-        nextMoveUp.appendChild(rarity);
-        currentPosition = nextMoveUp;
-        rarity.style.transform = "rotate(-90deg)" + "scaleX(-1)";
-        boxMoveUp.setAttribute("data-cell-type", "box");
-        boxMoveUp.classList.replace("blankSpace", "box");
-        } 
-        
-            break;
 
+            else if (moveUp === "box" && boxUp === "borderWall" || moveUp === "box" && boxUp === "box") {
+            }
+
+            else if (boxUp === "winLocations") {
+                nextMoveUp.classList.replace("box", "blankSpace");
+                nextMoveUp.setAttribute("data-cell-type", "blankSpace");
+                nextMoveUp.appendChild(rarity);
+                currentPosition = nextMoveUp;
+                rarity.style.transform = "rotate(-90deg)" + "scaleX(-1)";
+                boxMoveUp.setAttribute("data-cell-type", "boxOnWinLocation");
+                winCounter++;
+                boxMoveUp.classList.add("box")
+            }
+            else if (moveUp === "box") {
+                upMove(nextMoveUp, boxMoveUp)
+                boxMoveUp.setAttribute("data-cell-type", "box");
+                boxMoveUp.classList.replace("blankSpace", "box");
+            }
+            checkWin();
+            break;
+        //moves player down and pushes boxes
         case 'ArrowDown':
             let nextPositionDown = Number(rowPosition) + 1;
             let twoPositionDown = Number(rowPosition) + 2;
@@ -116,7 +158,6 @@ document.addEventListener('keydown', (event) => {
             }
 
             else if (moveDown === "box" && boxDown === "borderWall" || moveDown === "box" && boxDown === "box") {
-                console.log("stop")
             }
 
             else if (boxDown === "winLocations") {
@@ -124,22 +165,20 @@ document.addEventListener('keydown', (event) => {
                 nextMoveDown.setAttribute("data-cell-type", "blankSpace");
                 nextMoveDown.appendChild(rarity);
                 currentPosition = nextMoveDown;
-                rarity.style.transform = "rotate(0deg)";
-                boxMoveDown.setAttribute("data-cell-type", "box");
-                boxMoveDown.classList.replace("winLocations", "box")
+                rarity.style.transform = "rotate(90deg)" + "scaleX(-1)";
+                boxMoveDown.setAttribute("data-cell-type", "boxOnWinLocation");
+                winCounter++;
+                boxMoveDown.classList.add("box")
             }
 
             else if (moveDown === "box") {
-                nextMoveDown.classList.replace("box", "blankSpace");
-                nextMoveDown.setAttribute("data-cell-type", "blankSpace");
-                nextMoveDown.appendChild(rarity);
-                currentPosition = nextMoveDown;
-                rarity.style.transform = "rotate(90deg)" + "scaleX(-1)";
+                downMove(nextMoveDown, boxMoveDown)
                 boxMoveDown.setAttribute("data-cell-type", "box");
                 boxMoveDown.classList.replace("blankSpace", "box");
             }
+            checkWin();
             break;
-
+        //moves player left and pushes boxes
         case 'ArrowLeft':
             let nextPositionLeft = Number(cellPosition) - 1;
             let twoPositionLeft = Number(cellPosition) - 2;
@@ -153,8 +192,7 @@ document.addEventListener('keydown', (event) => {
                 rarity.style.transform = "rotate(0deg)"
             }
 
-            else if (moveLeft === "box" && boxLeft === "borderWall" || moveLeft === "box" && boxLeft === "box") {
-                console.log("Stop")
+            else if (moveLeft === "box" && boxLeft === "borderWall" || moveLeft === "box" && boxLeft === "box" || moveLeft === "boxOnWinLocation" && boxLeft === "borderWall") {//This stops the player from moving boxes through the wall or through other boxes.
             }
 
             else if (boxLeft === "winLocations") {
@@ -163,33 +201,38 @@ document.addEventListener('keydown', (event) => {
                 nextMoveLeft.appendChild(rarity);
                 currentPosition = nextMoveLeft;
                 rarity.style.transform = "rotate(0deg)";
-                boxMoveLeft.setAttribute("data-cell-type", "box");
-                boxMoveLeft.classList.replace("winLocations", "box")
+                boxMoveLeft.setAttribute("data-cell-type", "boxOnWinLocation");
+                winCounter++;
+                boxMoveLeft.classList.add("box")
             }
-            
+
             else if (boxLeft === "start") {
-                nextMoveLeft.classList.replace("box", "blankSpace");
-                nextMoveLeft.setAttribute("data-cell-type", "blankSpace");
-                nextMoveLeft.appendChild(rarity);
-                currentPosition = nextMoveLeft;
-                rarity.style.transform = "rotate(0deg)";
-                document.getElementById("start").removeAttribute("id"); 
+                leftMove(nextMoveLeft, boxMoveLeft)
+                document.getElementById("start").removeAttribute("id");
                 boxMoveLeft.classList.add("box");
                 boxMoveLeft.setAttribute("data-cell-type", "box");
             }
-            
-            else if (moveLeft === "box") {
-                nextMoveLeft.classList.replace("box", "blankSpace");
-                nextMoveLeft.setAttribute("data-cell-type", "blankSpace");
+
+            else if (moveLeft === "boxOnWinLocation") {
+                nextMoveLeft.classList.replace("box", "winLocations");
+                nextMoveLeft.setAttribute("data-cell-type", "winLocations");
                 nextMoveLeft.appendChild(rarity);
                 currentPosition = nextMoveLeft;
                 rarity.style.transform = "rotate(0deg)";
+                winCounter--;
                 boxMoveLeft.setAttribute("data-cell-type", "box");
                 boxMoveLeft.classList.replace("blankSpace", "box");
             }
+
+            else if (moveLeft === "box") {
+                leftMove(nextMoveLeft, boxMoveLeft);
+                boxMoveLeft.classList.replace("blankSpace", "box");
+                boxMoveLeft.setAttribute("data-cell-type", "box");
+            }
+            checkWin();
             break;
-            
-            case 'ArrowRight':
+        //moves player right and pushes boxes
+        case 'ArrowRight':
             let nextPositionRight = Number(cellPosition) + 1;
             let twoPositionRight = Number(cellPosition) + 2;
             let nextMoveRight = document.querySelector("[data-row-index = '" + rowPosition + "'][data-cell-index = '" + nextPositionRight + "']");
@@ -201,30 +244,34 @@ document.addEventListener('keydown', (event) => {
                 currentPosition = nextMoveRight;
                 rarity.style.transform = "scaleX(-1)"
             }
-            else if (moveRight === "box" && boxRight === "borderWall") {
-                console.log("Stop");
+            else if (moveRight === "box" && boxRight === "borderWall" || moveRight === "box" && boxRight === "box") {
             }
-            
+
             else if (boxRight === "winLocations") {
                 nextMoveRight.classList.replace("box", "blankSpace");
                 nextMoveRight.setAttribute("data-cell-type", "blankSpace");
                 nextMoveRight.appendChild(rarity);
                 currentPosition = nextMoveRight;
                 rarity.style.transform = "scaleX(-1)";
-                boxMoveRight.setAttribute("data-cell-type", "box");
-                boxMoveRight.classList.replace("winLocations", "box")
+                boxMoveRight.setAttribute("data-cell-type", "boxOnWinLocation");
+                winCounter++;
+                boxMoveRight.classList.add("box")
             }
 
             else if (moveRight === "box") {
-                nextMoveRight.classList.replace("box", "blankSpace");
-                nextMoveRight.setAttribute("data-cell-type", "blankSpace");
-                nextMoveRight.appendChild(rarity);
-                currentPosition = nextMoveRight;
-                rarity.style.transform = "scaleX(-1)";
-                boxMoveRight.setAttribute("data-cell-type", "box");
+                rightMove(nextMoveRight, boxMoveRight);
                 boxMoveRight.classList.replace("blankSpace", "box");
             }
+            checkWin();
             break;
-        }
     }
-);
+});
+
+function checkWin() {
+    if (winCounter == 7) {
+        console.log("Winner")
+    }
+    else {
+        console.log("Loser")
+    }
+}
